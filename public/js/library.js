f87d5bdee1958a7f2c19780d6c8c906cfd4874d1
jkenyon1 <62671170+jkenyon1@users.noreply.github.com>
2020-12-10 15:06:30 -0500

    added Album art to DB
    added url for album art
    utilize in feed so it shows the song art

--- public/js/library.js
+++ public/js/library.js
@@ -7,7 +7,7 @@ $(document).ready(() => {
         var id = data.id;
 
         // Call to Get the user's music from the DB using their ID and then render results
-        $.get("/api/songs/" + id).then(songs => {
+        $.get("/api/songs/"+id).then(songs => {
             console.log(songs);
       
 
